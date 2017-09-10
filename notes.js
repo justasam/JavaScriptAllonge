8963c7dc7d2e7e2ab8df3e61cabfe940f0ce2093
justasam <samuolis.just@gmail.com>
2017-09-10 20:22:24 +0100

    Added Partial Application recipes

---
+++ notes.js
@@ -0,0 +1,23 @@
+// Partial Application
+const callFirst = (fn, larg) =>
+  function (...rest) {
+    return fn.call(this, larg, ...rest);
+  }
+
+const callLast = (fn, rarg) =>
+  function (...rest) {
+    return fn.call(this, ...rest, rarg);
+  }
+
+const greet = (me, you) =>
+  `Hello, ${you}, my name is ${me}`;
+
+const callLeft = (fn, ...args) =>
+  (...remainingArgs) =>
+    fn(...args, ...remainingArgs);
+
+const callRight = (fn, ...args) =>
+  (...remainingArgs) =>
+    fn(...remainingArgs, ...args);
+
+    
