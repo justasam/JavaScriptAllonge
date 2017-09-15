0563be52bb5e147d033950de4c5f069ace0a01f2
justasam <samuolis.just@gmail.com>
2017-09-15 19:53:03 +0100

    Added a flatten function for flattening lists

--- notes.js
+++ notes.js
@@ -87,3 +87,16 @@ at(
     )
   }
 }
+
+// Flatten
+const flatten = ([first, ...rest]) => {
+  if (first === undefined) {
+    return [];
+  }
+  else if (!Array.isArray(first)) {
+    return [first, ...flatten(rest)];
+  }
+  else {
+    return [...flatten(first), ...flatten(rest)];
+  }
+}
